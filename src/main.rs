74538b3b9066eb2e4349faf90b8552d93c84a10d
Underslash12 <stew_smith@hotmail.com>
2024-06-28 01:36:23 -0700

    Added -d and -F arguments and cleaned up errors

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,31 @@
 // main.rs
 
+#![feature(io_error_more)]
+
+use std::fs::File;
+use std::path::{Path, PathBuf};
+use std::io::{Error as IoError, ErrorKind as IoErrorKind, Write};
+
 const HELP: &'static str = "
 Usage: python-su [options] ... 
 
 Options:
-  -h, --help                Prints the help message
+  -h, --help                Prints this help message
   -d, --dir <DIRECTORY>     Create project in the specified directory, 
-                                default is the working directory
+                                by default this is the working directory
   -f, --folder <FOLDER>     Create project files in the specified folder,
-                                default is the same as the name
-  -F                        Use the working directory as the project folder
+                                by default this is the project name
+  -F                        Use the directory as the project folder
+                                this should not be used with the -f flag
 
 Arguments:
-  name                      Name of the project 
-"; 
+  name                      Name of the project"; 
 
 #[derive(Debug)]
 struct AppArgs {
     directory: Option<String>,
     folder: Option<String>,
-    project_folder_is_parent: bool,
+    project_folder_is_directory: bool,
     project_name: String,
 }
 
@@ -27,12 +33,13 @@ struct AppArgs {
 fn parse_args() -> Result<AppArgs, pico_args::Error> {
     let mut pargs = pico_args::Arguments::from_env();
 
-    // Help has a higher priority and should be handled separately.
+    // help has a higher priority and should be handled separately.
     if pargs.contains(["-h", "--help"]) {
         println!("{}", HELP);
         std::process::exit(0);
     }
 
+    // create an AppArgs struct from the arguments, erroring if not possible
     let args: AppArgs = AppArgs {
         directory: match pargs.opt_value_from_str("-d")? {
             Some(t) => Some(t),
@@ -42,7 +49,7 @@ fn parse_args() -> Result<AppArgs, pico_args::Error> {
             Some(t) => Some(t),
             None => pargs.opt_value_from_str("--folder")?,
         },
-        project_folder_is_parent: pargs.contains("-F"),
+        project_folder_is_directory: pargs.contains("-F"),
         project_name: pargs.free_from_str()?,
     };
 
@@ -57,14 +64,73 @@ fn parse_args() -> Result<AppArgs, pico_args::Error> {
     Ok(args)
 }
 
-// create the parent project folder if it needs to be created
-fn create_project_folder(args: &AppArgs) {
+// create and return the pathbuf to the project folder
+fn create_project_folder(args: &AppArgs) -> Result<PathBuf, IoError> {
+    // get the working directory
+    let cwd = std::env::current_dir()?;
+    
+    // convert the directory into a pathbuf struct
+    let mut dir: PathBuf = match &args.directory {
+        Some(d) => {
+            let path = Path::new(&d);
+            if path.is_absolute() {
+                path.to_path_buf()
+            } else {
+                let mut p = cwd.to_path_buf();
+                p.push(path);
+                p
+            }
+        },
+        None => std::env::current_dir()?,
+    };
 
+    // create the folder if it doesn't already exist
+    if !dir.try_exists()? {
+        std::fs::create_dir_all(dir.clone())?;
+    } else if dir.is_file() {
+        // if the directory exists but is a file, error
+        return Err(IoError::new(IoErrorKind::NotADirectory, "Specified directory is a file"));
+    }
+
+    // need to create another folder if the project folder is not the directory
+    if !args.project_folder_is_directory {
+        // append the project folder to the directory
+        dir.push(
+            match &args.folder {
+                Some(f) => Path::new(f),
+                None => Path::new(&args.project_name),
+            } 
+        );
+
+        // create the folder, error if it already exists or is a file
+        if !dir.try_exists()? {
+            // create the new directories
+            std::fs::create_dir_all(dir.clone())?;
+        } else if dir.is_file() {
+            // if the folder exists but is a file, error
+            return Err(IoError::new(IoErrorKind::NotADirectory, "Specified folder is a file"));
+        } else {
+            // if the folder already exists, still error
+            return Err(IoError::new(IoErrorKind::AlreadyExists, "Specified folder already exists"));
+        }
+    }
+    
+    Ok(dir)
 }
 
 // create the base files
-fn create_base_project_files(args: &AppArgs) {
+fn create_base_project_files(args: &AppArgs, path: &PathBuf) -> Result<(), IoError> {
+    let mut py_path = path.clone();
+    py_path.push(&std::format!("{}.py", &args.project_name));
+    let mut py_file = File::create_new(py_path)?;
+    write!(py_file, "# {}.py\n", &args.project_name)?;
 
+    let mut bat_path = path.clone();
+    bat_path.push(&std::format!("{}.bat", &args.project_name));
+    let mut bat_file = File::create_new(bat_path)?;
+    write!(bat_file, "python {}.py\npause", &args.project_name)?;
+
+    Ok(())
 }
 
 fn main() {
@@ -77,8 +143,22 @@ fn main() {
         }
     };
 
-    println!("{:?}", args);
+    // println!("{:?}", args);
 
-    create_project_folder(&args);
-    create_base_project_files(&args);
+    // create the project directory and folder
+    let path = match create_project_folder(&args) {
+        Ok(p) => p,
+        Err(e) => {
+            eprintln!("Error: {}.", e);
+            eprintln!("{}", HELP);
+            std::process::exit(1);
+        } 
+    };
+
+    // generate project files
+    if let Err(e) = create_base_project_files(&args, &path) {
+        eprintln!("Error: {}.", e);
+        eprintln!("{}", HELP);
+        std::process::exit(1);
+    }
 }
